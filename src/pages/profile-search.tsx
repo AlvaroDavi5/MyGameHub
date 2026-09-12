5e86d3a9a4f976d41172229107eee6e698720134
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2026-09-12 19:36:35 -0300

    feat: implement ProfileSearch component with username input and states handling

--- src/pages/profile-search.tsx
+++ src/pages/profile-search.tsx
@@ -1,4 +1,5 @@
-import { Box } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Box, Button, Flex, Input } from '@chakra-ui/react';
 import type { Route } from './+types/home';
 
 export function meta(_args: Route.MetaArgs) {
@@ -6,5 +7,48 @@ export function meta(_args: Route.MetaArgs) {
 }
 
 export default function ProfileSearch() {
-	return <Box>Profile Search</Box>;
+	const [inputUsername, setInputUsername] = useState<string | null>(null);
+	const [inputUsernameValue, setInputUsernameValue] = useState<string>('');
+	const [disableSearchButton, setDisableSearchButton] = useState<boolean>(true);
+
+	useEffect(() => {
+		if (inputUsername) {
+			console.log('Usuário pesquisado:', inputUsername);
+		}
+
+		return () => {
+			setDisableSearchButton(true);
+		};
+	}, [inputUsername]);
+
+	const inputUsernameChange = (inputValue: string): void => {
+		const trimmedValue = inputValue.trim();
+		const wasChanged = trimmedValue !== inputUsername?.trim() && trimmedValue !== '';
+
+		setInputUsernameValue(inputValue);
+		setDisableSearchButton(!wasChanged);
+	};
+	const submitSearch = (): void => {
+		setInputUsername(inputUsernameValue);
+	};
+
+	return (
+		<>
+			<Flex
+				aria-label="input-element"
+				direction={['column', 'row']}
+				alignItems={['flex-start', 'center']}
+				justifyContent={['flex-start', 'center']}
+				gap={[2, 4]}
+				padding={4}
+			>
+				<Input value={inputUsernameValue} onChange={(e) => inputUsernameChange(e.target.value)} placeholder="Digite o username" width={['100%', '600px']} />
+				<Button onClick={submitSearch} disabled={disableSearchButton}>
+					Pesquisar
+				</Button>
+			</Flex>
+
+			<Box>Resultados da pesquisa: {inputUsername}</Box>
+		</>
+	);
 }
